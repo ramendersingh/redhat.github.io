acb16972f9946a7f0c082ccb0b7abc5679b04343
ram_git <raminder388@gmail.com>
2023-02-23 09:44:25 +0530

    comt1

---
+++ abc.js
@@ -0,0 +1,14 @@
+"//how to make login page in html?"
+var pwArray = ["Password1", "Password2", "Password3", "Password4"];
+
+
+var pwLocation = ["location1", "location2", "location3", "location4"];
+window.location = pwLocation[i];
+
+
+
+
+//Source: https://stackoverflow.com/questions/20172836
+
+
+
